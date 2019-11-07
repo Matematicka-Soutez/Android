ea976b713b578e301362dda36e4f9f2a6ea036d1
Jiri Erhart <jiri.erhart@strv.com>
2019-11-07 19:43:54 +0100

    Sync game code range with web app

--- app/src/main/java/cz/cuni/mff/maso/ui/settings/SettingsViewModel.kt
+++ app/src/main/java/cz/cuni/mff/maso/ui/settings/SettingsViewModel.kt
@@ -5,13 +5,14 @@ import androidx.lifecycle.Transformations
 import cz.cuni.mff.maso.tools.Preferences
 import cz.cuni.mff.maso.ui.BaseViewModel
 
-private const val GAME_CODE_MIN_LENGTH = 6
+private const val GAME_CODE_MIN_LENGTH = 4
+private const val GAME_CODE_MAX_LENGTH = 16
 
 class SettingsViewModel : BaseViewModel() {
 
 	val usePrinter = MutableLiveData<Boolean>()
 	val gameCode = MutableLiveData<String>()
-	val isGameCodeValid: MutableLiveData<Boolean> = Transformations.map(gameCode) { gameCode.value?.length ?: 0 >= GAME_CODE_MIN_LENGTH } as MutableLiveData<Boolean>
+	val isGameCodeValid: MutableLiveData<Boolean> = Transformations.map(gameCode) { gameCode.value?.length ?: 0 in GAME_CODE_MIN_LENGTH..GAME_CODE_MAX_LENGTH } as MutableLiveData<Boolean>
 
 	fun updateData(): Boolean {
 		Preferences.setUsePrinter(usePrinter.value == true)
