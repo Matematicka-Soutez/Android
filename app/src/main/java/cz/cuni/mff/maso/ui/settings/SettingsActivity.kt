6c80f7206cca06d27a53182f8a70f5be6a706ca9
Jonas Havelka <jonas.havelka@moznabude.cz>
2019-11-07 22:48:05 +0100

    Added Bluetooth

--- app/src/main/java/cz/cuni/mff/maso/ui/settings/SettingsActivity.kt
+++ app/src/main/java/cz/cuni/mff/maso/ui/settings/SettingsActivity.kt
@@ -1,7 +1,11 @@
 package cz.cuni.mff.maso.ui.settings
 
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
@@ -17,6 +21,8 @@ interface SettingsView {
 }
 
 private const val ARG_CHANGE_SETTINGS = "arg_change_settings"
+private const val REQUEST_ENABLE_BT = 11
+private val BA = BluetoothAdapter.getDefaultAdapter()
 
 class SettingsActivity : BaseActivity<ActivitySettingsBinding, SettingsViewModel, SettingsView>() {
 	override val layoutResId: Int = R.layout.activity_settings
@@ -24,7 +30,18 @@ class SettingsActivity : BaseActivity<ActivitySettingsBinding, SettingsViewModel
 	override val view = object : SettingsView {
 		override fun onNextClicked() {
 			if (viewModel.updateData()) {
-				startActivity(QrScanActivity.newIntent(this@SettingsActivity))
+
+				if (viewModel.usePrinter.value != null && viewModel.usePrinter.value!!) {
+					if (!BA.isEnabled) {
+						val turnOn = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+						startActivityForResult(turnOn, REQUEST_ENABLE_BT)
+					} else {
+						findDevices()
+					}
+
+				} else {
+					startQrScanActivity()
+				}
 			}
 		}
 	}
@@ -47,6 +64,11 @@ class SettingsActivity : BaseActivity<ActivitySettingsBinding, SettingsViewModel
 		if (!displayBackArrow() && !Preferences.getGameCode().isNullOrEmpty()) {
 			startActivity(QrScanActivity.newIntent(this))
 		}
+		val filter = IntentFilter()
+		filter.addAction(BluetoothDevice.ACTION_FOUND)
+		filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED)
+		filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED)
+		registerReceiver(receiver, filter)
 	}
 
 	override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -72,5 +94,58 @@ class SettingsActivity : BaseActivity<ActivitySettingsBinding, SettingsViewModel
 
 	private fun startLoginActivity() {
 		startActivity(LoginActivity.newIntent(this))
+	}
+
+	private fun startQrScanActivity() {
+		startActivity(QrScanActivity.newIntent(this@SettingsActivity))
+	}
+
+	private fun findDevices() {
+		if (BA.isDiscovering) {
+			BA.cancelDiscovery()
+		}
+		BA.startDiscovery()
+		println("A")
+	}
+
+	override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+		super.onActivityResult(requestCode, resultCode, data)
+		if (requestCode == REQUEST_ENABLE_BT) {
+			findDevices()
+		}
+	}
+
+	// Create a BroadcastReceiver for ACTION_FOUND.
+	private val receiver = object : BroadcastReceiver() {
+
+		override fun onReceive(context: Context, intent: Intent) {
+			val action: String = intent.action.toString()
+			println(action)
+			when(action) {
+				BluetoothDevice.ACTION_FOUND -> {
+					// Discovery has found a device. Get the BluetoothDevice
+					// object and its info from the Intent.
+					val device: BluetoothDevice =
+						intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)!!
+					val deviceName = device.name
+					val deviceHardwareAddress = device.address // MAC address
+					if (deviceName.startsWith("Maso ")) {
+						//device.setPin(362940)
+						device.createBond()
+					}
+				}
+				BluetoothAdapter.ACTION_DISCOVERY_STARTED -> {
+					println("Started")
+				}
+				BluetoothAdapter.ACTION_DISCOVERY_FINISHED -> {
+					println("Ended")
+				}
+			}
+		}
+	}
+
+	override fun onDestroy() {
+		super.onDestroy()
+		unregisterReceiver(receiver)
 	}
 }
